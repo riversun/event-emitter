71f18c12943aa40469069ff01b73be4e3aa4b083
riversun <riversun.org@gmail.com>
2020-02-14 19:06:49 +0900

    update ver. to 1.1 remove webpack dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,4 @@
 module.exports = {
-    presets: [
-        [
-            '@babel/preset-env',
-            {
-                'modules': 'false',//commonjs,amd,umd,systemjs,auto
-                'useBuiltIns': 'usage',
-                'targets': '> 0.25%, not dead',
-                'corejs': 3
-            }
-        ]
-    ],
     env: {
         test: {
             presets: [['@babel/preset-env', {targets: {node: 'current'}}]],
